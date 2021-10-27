cf45ef03b30c5a641c68b5a9157cf9cc95030ce2
Chris Harris <33553721+cjharris18@users.noreply.github.com>
2021-10-27 19:25:04 +0100

    Optimise and Automate Image Repairing
    I have optimized the bruteforcer to be much simpler. I have also added the following:
    - Verbose mode.
    - Automate finding the crc.
    - Automate repairing the image and outputting it to `fixed.png`.
    - Allow output option for the user to specify a file output.

--- png_dimensions_bruteforce.py
+++ png_dimensions_bruteforce.py
@@ -2,43 +2,52 @@
 
 # Created by Christopher Harris (cjharris18).
 # https://github.com/cjharris18
+# Credit to jdb_jdb for helping optimise me optimise this further.
 
 from zlib import crc32
 import argparse
-import sys
+import struct
 
 # Argument Handler.
 parser = argparse.ArgumentParser(description='Brute force the image dimensions of a PNG image.')
-parser._action_groups.pop()
 required = parser.add_argument_group('Required Arguments')
+optional = parser.add_argument_group('Optional Arguments')
 
 required.add_argument('FILE', help='The filename of the png image you want to bruteforce.')
-required.add_argument('-c', '--checksum', help='The target checksum for the bruteforcer.', required=True)
+optional.add_argument('-v', '--verbose', help='Verbose mode.', action='store_true')
+optional.add_argument('-o', '--output', help='Output the fixed png to a different filename.')
 args = parser.parse_args()
 
 # Print a nice message to the user.
-print("="*51)
-print(" "*4 + "**" + " "*4 + "PNG Image Dimension Bruteforcer" + " "*4 + "**")
-print(" "*15 + "Created by cjharris18")
-print("="*51 + "\n")
+print('='*51)
+print(' '*4 + '**' + ' '*4 + 'PNG Image Dimension Bruteforcer' + ' '*4 + '**')
+print(' '*15 + 'Created by cjharris18')
+print('='*51 + '\n')
 
-data = open(args.FILE, 'rb').read()
-index = 12
+png = bytearray(open(args.FILE, 'rb').read())
 
-ihdr = bytearray(data[index:index+17]) #ihdr
-width_index = 7 #width
-height_index = 11 #height
+# Pull the crc
+crcStart = 29
+crcTarget = (bytearray(png[crcStart:crcStart+4])).hex()
+crcTarget = '0x' + str(crcTarget)[:]
 
-for x in range (1,2000):
-    height = bytearray(x.to_bytes(2,'big'))
-    for y in range(1,2000):
-        width = bytearray(y.to_bytes(2,'big'))
-        for h in range(len(height)):
-            ihdr[height_index - h] = height[-h -1]
-        for w in range(len(width)):
-            ihdr[width_index - w] = width[-w -1]
-        if hex(crc32(ihdr)) == args.checksum:
-            print("Found Correct Dimensions...\nWidth: {}\nHeight: {}".format(width.hex(),height.hex()))
-            sys.exit()
-        for i in range(len(width)):
-            ihdr[width_index - i] = bytearray(b'\x00')[0]
+for width in range(1000):
+    for height in range(1000):
+
+        png[0x10:0x14] = struct.pack(">I",width)
+        png[0x14:0x18] = struct.pack(">I",height)
+
+        calculatedCrc = hex(crc32(png[12:29]))
+        if calculatedCrc == crcTarget:
+            if args.verbose == True:
+                print('Found Correct Dimensions...\nWidth: {}\nHeight: {}'.format(hex(width),hex(height)))
+                print('\nRemember to pad this with leading 0\'s as required.')
+            if ('output' in args) == True:
+                with open(args.output,'wb') as file:
+                    file.write(png)
+                    print('\nSuccessfully wrote to: {}'.format(args.overwrite))
+            else:
+                with open('fixed.png','wb') as file:
+                    file.write(png)
+                    print('\nSuccessfully wrote to: fixed.png')
+            break
